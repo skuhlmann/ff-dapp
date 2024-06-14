294aad6355779cc9c40707db4490f8cc2ea42a25
skuhlmann <samkuhlmann@gmail.com>
2024-06-14 15:24:44 -0600

    adds link to rarible

--- src/components/ListingList.tsx
+++ src/components/ListingList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Heading } from "@chakra-ui/react";
+import { Flex, Box, Heading, Link, Text } from "@chakra-ui/react";
 import type { Item } from "@rarible/api-client";
 import { ListingCard } from "./ListingCard";
 
@@ -14,8 +14,16 @@ export const ListingList = ({ listings }: { listings: Item[] }) => {
       >
         {listings.length < 1 && (
           <Flex gap="1rem" direction="column" align="center" justify="center">
-            <Heading color="brand.orange">Market Sold Out</Heading>
+            <Heading color="brand.orange">Market Is Almost Open</Heading>
             <Heading color="brand.orange">Check back soon</Heading>
+            <Link
+              href="https://rarible.com/collection/base/0x5eae1344f40f25b827782aff0b3651b2dcd2259e/items"
+              isExternal
+            >
+              <Text color="brand.blue">
+                You can view current listings in the Rarible marketplace
+              </Text>
+            </Link>
           </Flex>
         )}
         {listings.map((item: Item) => {
