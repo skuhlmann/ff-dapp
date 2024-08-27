8781fb98a4c5983b3f025871be793b23fdb8a2a5
skuhlmann <samkuhlmann@gmail.com>
2024-08-27 16:30:59 -0600

    adds banner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Footer } from "./Footer";
 
 export const Layout = () => {
   return (
     <>
+      <Flex
+        as="nav"
+        align="center"
+        justify="center"
+        w="100%"
+        textAlign="center"
+        backgroundColor="brand.green"
+        position="fixed"
+        zIndex={10}
+        px={8}
+      >
+        <Text fontSize="md" py="1rem" fontWeight="700">
+          The 2024 Peach Season is coming to an end. Get your orders in now, the
+          redemption window will close at 9am MDT/3pm UTC on August 31st.
+        </Text>
+      </Flex>
       <NavBar />
-      <Box pt="120px">
+      <Box pt="180px">
         <Outlet />
       </Box>
       <Footer />
